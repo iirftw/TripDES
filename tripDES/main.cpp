89be61c2e9f96c75e12375e13e02d37004b93406
Andrew Chichak <andrew@chichak.ca>
2017-09-30 11:22:25 -0600

    Initial Commit

---
+++ tripDES/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tripDES
+//
+//  Created by Andrew Chichak on 2017-09-30.
+//  Copyright © 2017 Andrew Chichak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
